7860fce108bea328fd6d9c9c810f53b467c4254c
ryanperretta6 <ryan.perretta@gmail.com>
2021-12-15 14:50:11 -0500

    altered app title

--- image-mosaic-app/src/components/App.js
+++ image-mosaic-app/src/components/App.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import '../App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Account from './Account';
-import CreateMosaic from './CreateMosaic';
-import Navigation from './Navigation';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import { AuthProvider } from '../firebase/Auth';
-import PrivateRoute from './PrivateRoute';
+import React from "react";
+import "../App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Account from "./Account";
+import CreateMosaic from "./CreateMosaic";
+import Navigation from "./Navigation";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import { AuthProvider } from "../firebase/Auth";
+import PrivateRoute from "./PrivateRoute";
 function App() {
-	return (
-		<AuthProvider>
-			<Router>
-				<div className="App">
-					<header className="App-header">
-						<Navigation />
-					</header>
-				</div>
-				<Route exact path="/" component={CreateMosaic} />
-				{/* <PrivateRoute path="/home" component={} /> */}
-				<PrivateRoute path="/account" component={Account} />
-				<Route path="/signin" component={SignIn} />
-				<Route path="/signup" component={SignUp} />
-			</Router>
-		</AuthProvider>
-	);
+    return (
+        <AuthProvider>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <Link to="/">
+                            <h1 id="appTitle">Image Mosaic Creator</h1>
+                        </Link>
+                        <Navigation />
+                    </header>
+                </div>
+                <Route exact path="/" component={CreateMosaic} />
+                {/* <PrivateRoute path="/home" component={} /> */}
+                <PrivateRoute path="/account" component={Account} />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/signup" component={SignUp} />
+            </Router>
+        </AuthProvider>
+    );
 }
 
 export default App;
