114c74daa07e042ad2822b4284f8759b462f277d
ryanperretta6 <ryan.perretta@gmail.com>
2021-12-19 13:53:10 -0500

    installed

--- image-mosaic-app/src/imageCheck.ts
+++ image-mosaic-app/src/imageCheck.ts
@@ -1,8 +1,7 @@
-function imageCheck(fileType: string): boolean{
+function imageCheck(fileType: string): boolean {
     let prefix: string = fileType.split("/")[0];
 
-    if(prefix !== "image")
-        return false
+    if (prefix !== "image") return false;
     return true;
 }
 
